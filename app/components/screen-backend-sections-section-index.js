514d58948c76d023e6657c85d9865c09b87b2071
ampatspell <ampatspell@gmail.com>
2016-10-12 21:37:13 +0300

    ui-backend-row-value and show placeholder index

--- app/components/screen-backend-sections-section-index.js
+++ app/components/screen-backend-sections-section-index.js
@@ -1,7 +1,19 @@
 import Ember from 'ember';
 
+const {
+  computed
+} = Ember;
+
 export default Ember.Component.extend({
   classNameBindings: [ ':screen-backend-sections-section-index' ],
+
+  componentName: computed('section.modelName', function() {
+    let section = this.get('section');
+    if(!section) {
+      return;
+    }
+    return `screen-backend-sections-section-index-${section.get('modelName')}`;
+  }).readOnly(),
 
   actions: {
     delete() {
