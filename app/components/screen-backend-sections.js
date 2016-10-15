8d73dab70c0d5369ed7d2272ba54fc395d4bdb1a
ampatspell <ampatspell@gmail.com>
2016-10-15 22:47:05 +0300

    ui-backend-tree removed

--- app/components/screen-backend-sections.js
+++ app/components/screen-backend-sections.js
@@ -1,28 +1,5 @@
 import Ember from 'ember';
 
-const {
-  computed
-} = Ember;
-
 export default Ember.Component.extend({
-  classNameBindings: [ ':screen-bakend-sections' ],
-
-  roots: computed('sections.@each.{category,position}', {
-    get() {
-      return this.get('sections').filterBy('category', null).sortBy('position');
-    },
-    set(key, value) {
-      value.forEach((model, idx) => {
-        model.set('position', idx);
-      });
-      return value;
-    }
-  }),
-
-  actions: {
-    sort(array) {
-      array.map(model => model.save());
-    }
-  }
-
+  classNameBindings: [ ':screen-bakend-sections' ]
 });
