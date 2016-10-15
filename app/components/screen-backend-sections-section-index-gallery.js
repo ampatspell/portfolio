b924aef389917a19d02665669c976ec8f7af8cc3
ampatspell <ampatspell@gmail.com>
2016-10-15 23:00:40 +0300

    reorder grid also with the same reorerable

--- app/components/screen-backend-sections-section-index-gallery.js
+++ app/components/screen-backend-sections-section-index-gallery.js
@@ -1,25 +1,31 @@
 import Ember from 'ember';
 
 const {
-  computed,
-  copy,
   RSVP: { all }
 } = Ember;
 
 export default Ember.Component.extend({
   classNameBindings: [ ':screen-backend-sections-section-index-text' ],
 
-  // TODO: hasMany is not sortable. this is overwritten on 1st reorder
-  sortable: computed('section.sortedImages.@each.position', function() {
-    return this.get('section.sortedImages');
-  }),
-
   actions: {
-    sort() {
-      all(this.get('sortable').map((image, idx) => {
-        image.set('position', idx);
-        return image.save();
-      }));
+    reorder(model, pos, relative) {
+      console.log('reorder', model+'', pos, relative+'');
+
+      let array = this.get('section.images').sortBy('position');
+      array.removeObject(model);
+
+      let idx = array.indexOf(relative);
+      if(pos === 'before') {
+        array.insertAt(idx, model);
+      } else {
+        array.insertAt(idx + 1, model);
+      }
+
+      array.forEach((model, idx) => {
+        model.set('position', idx);
+      });
+
+      all(array.map(model => model.save()));
     },
     upload() {
       this.transitionTo('backend.sections.section.gallery.upload', this.get('section'));
