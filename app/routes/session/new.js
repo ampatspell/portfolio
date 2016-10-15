ca18062e1f2dcfa9ffcc7b6219afb47c96a4061e
ampatspell <ampatspell@gmail.com>
2016-10-16 02:58:22 +0300

    screen-backend/session-new

--- app/routes/session/new.js
+++ app/routes/session/new.js
@@ -1,17 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.get('store.session');
-  },
 
-  actions: {
-    save() {
-      this.currentModel.save().then(() => {
-        this.transitionTo('backend');
-      }, err => {
-        console.log(err);
-      });
+  beforeModel() {
+    if(!this.get('store.session.isAuthenticated')) {
+      return;
     }
+    this.transitionTo('index');
   }
+
 });
