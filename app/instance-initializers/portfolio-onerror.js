092686e6705ff2ecac824324a54d1888fb1a2431
ampatspell <ampatspell@gmail.com>
2016-10-18 18:07:38 +0300

    jshint

--- app/instance-initializers/portfolio-onerror.js
+++ app/instance-initializers/portfolio-onerror.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default {
   name: 'portfolio:onerror',
   initialize(app) {
-    Ember.onerror = function(err, opts) {
+    Ember.onerror = function(err) {
       err = err || new Ember.Error('onerror');
       let service = app.lookup('service:error-reporter');
       service.report(err);
