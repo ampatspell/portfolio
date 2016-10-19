ac66c6ceb946916baf0eceb1a994e20825c9b8bb
ampatspell <ampatspell@gmail.com>
2016-10-19 15:20:51 +0300

    "rutemartajansone" site

---
+++ sites/rutemartajansone.js
@@ -0,0 +1,6 @@
+module.exports = Object.assign({
+  title:    'Rute Marta Jansone',
+  theme:    'rutemartajansone',
+  admin:    'portfolio-rutemartajansone-admin',
+  database: 'portfolio-rutemartajansone'
+}, require('./base'));
