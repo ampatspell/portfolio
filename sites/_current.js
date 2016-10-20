23110210f5fc510eb29081751314ff7689af6589
ampatspell <ampatspell@gmail.com>
2016-10-20 06:12:17 +0300

    default to 'default' site. print out site name

--- sites/_current.js
+++ sites/_current.js
@@ -1,11 +1,13 @@
 module.exports = (() => {
 
-  let name = process.env.SITE;
+  let name = process.env.SITE || 'default';
 
   if(!name) {
     console.error('Usage: SITE=yoursitename ember ...');
     process.exit(-1);
   }
+
+  console.log(`Site: ${name}`);
 
   return require(`./${name}`);
 })();
