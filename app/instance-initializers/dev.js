a95475fc77105a680388e46d42369c780fdb2c14
ampatspell <ampatspell@gmail.com>
2016-10-22 23:12:02 +0300

    migration removed

--- app/instance-initializers/dev.js
+++ app/instance-initializers/dev.js
@@ -6,34 +6,5 @@ export default {
     window.db = db;
     window.store = store;
     window.log = console.log.bind(console);
-
-    window.migrate = () => {
-      let docs = db.get('documents');
-      return db.insertDesignDocuments().then(() => {
-        return docs.view('main', 'section', { include_docs: true });
-      }).then(json => {
-        let all = json.rows.map(item => item.doc);
-        return Ember.RSVP.all(all.map(doc => {
-
-          if(doc.title) {
-            let title = doc.title;
-            delete doc.title;
-            doc.page_title = title;
-          }
-
-          let category = doc.category;
-          if(category && typeof category === 'string') {
-            let cat = all.findBy('_id', category);
-            let type = cat.type.split(':')[1];
-            doc.category = {
-              id: cat._id,
-              type
-            };
-          }
-
-          return docs.save(doc);
-        }));
-      });
-    };
   }
 };
