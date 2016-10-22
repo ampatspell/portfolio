bfedf5448fe3edae8e0d9e6404bbf230f0890c93
ampatspell <ampatspell@gmail.com>
2016-10-22 23:11:56 +0300

    get rid of isCategory prop

--- app/models/category.js
+++ app/models/category.js
@@ -1,12 +1,9 @@
-import Ember from 'ember';
 import { prefix, type } from 'sofa';
 import Section from './section';
 
 export default Section.extend({
 
   id: prefix('section:'),
-  type: type('section:category'),
-
-  isCategory: true
+  type: type('section:category')
 
 });
