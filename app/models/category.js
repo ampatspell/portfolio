60fa525a9a25673d21df2f553a9fded201966121
ampatspell <ampatspell@gmail.com>
2016-10-23 21:10:31 +0300

    category.open editing and show in backend

--- app/models/category.js
+++ app/models/category.js
@@ -1,9 +1,11 @@
-import { prefix, type } from 'sofa';
+import { prefix, type, attr } from 'sofa';
 import Section from './section';
 
 export default Section.extend({
 
   id: prefix('section:'),
-  type: type('section:category')
+  type: type('section:category'),
+
+  open: attr('boolean'),
 
 });
