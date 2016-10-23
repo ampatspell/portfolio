7f7ca6c4bc080b5e9ce7ff65700ecd382f60efd3
ampatspell <ampatspell@gmail.com>
2016-10-23 23:34:49 +0300

    responsive stuff for ios and tablet

--- app/components/screen-backend/sections.js
+++ app/components/screen-backend/sections.js
@@ -1,7 +1,16 @@
+import Ember from 'ember';
 import Base from './sections/-base';
+
+const {
+  inject: { service },
+  computed: { equal },
+} = Ember;
 
 export default Base.extend({
   classNameBindings: [ ':screen-backend-sections' ],
+
+  router: service(),
+  isIndex: equal('router.currentRouteName', 'backend.sections.index'),
 
   sections: null,
 
