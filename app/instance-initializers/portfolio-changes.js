54bd5cf6f0e8609f25d0235c5a2545c35709c4b7
ampatspell <ampatspell@gmail.com>
2017-06-16 19:38:31 +0300

    console.log removed

--- app/instance-initializers/portfolio-changes.js
+++ app/instance-initializers/portfolio-changes.js
@@ -3,7 +3,6 @@ export default {
   after: 'portfolio:injections',
   initialize(app) {
     let changes = app.lookup('service:store').get('db.main').changes('main');
-    changes.on('change', change => console.log(change));
     changes.start();
   }
 };
