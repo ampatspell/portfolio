069b5c3de31d472ca46935a8a9702af86116234e
ampatspell <ampatspell@gmail.com>
2017-07-19 18:02:25 +0300

    bring back backend session and loadNested

--- app/routes/backend.js
+++ app/routes/backend.js
@@ -3,12 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   beforeModel() {
-    // let session = this.get('store.session');
-    // if(!session.get('isAuthenticated')) {
-    //   this.transitionTo('session.new');
-    // } else if(!session.get('isAdmin')) {
-    //   return this.transitionTo('index');
-    // }
+    let session = this.get('store.session');
+    if(!session.get('isAuthenticated')) {
+      this.transitionTo('session.new');
+    } else if(!session.get('isAdmin')) {
+      return this.transitionTo('index');
+    }
     this.get('store.db.main').deselectSections();
   }
 
--- app/routes/backend/sections/section/index.js
+++ app/routes/backend/sections/section/index.js
@@ -4,8 +4,7 @@ export default Ember.Route.extend({
 
   model() {
     let section = this.modelFor('backend.sections.section');
-    return section;
-    // return section.loadNested().then(() => section);
+    return section.loadNested().then(() => section);
   }
 
 });
