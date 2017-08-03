96b32b2e87eea5bc10d7edf2488a5d6edc5b4208
ampatspell <ampatspell@gmail.com>
2017-08-03 23:49:59 +0300

    update deps, use middleware instead of fastboot-app-server

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const FastBootAppServer = require('fastboot-app-server');
+const path = require('path');
+const express = require('express');
+const fastboot = require('fastboot-express-middleware');
 
-let server = new FastBootAppServer({
-  distPath: 'dist',
-  gzip: true
+let dist = path.join(__dirname, 'dist');
+
+let app = express();
+
+app.use(express.static(dist));
+app.get('/*', fastboot(dist));
+
+app.listen(3000, () => {
+  console.log('FastBoot app listening on port 3000');
 });
-
-server.start();
