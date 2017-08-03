dae5a5739aae292a75a6343a1fd3cf0dc7f1d1fd
ampatspell <ampatspell@gmail.com>
2017-08-04 00:16:28 +0300

    fix

--- app/instance-initializers/portfolio-changes.js
+++ app/instance-initializers/portfolio-changes.js
@@ -2,7 +2,8 @@ export default {
   name: 'portfolio:changes',
   after: 'portfolio:injections',
   initialize(app) {
-    if(!app.lookup('service:fastboot').get('isFastBoot')) {
+    let fastboot = app.lookup('service:fastboot');
+    if(fastboot.get('isFastBoot')) {
       return;
     }
     let changes = app.lookup('service:store').get('db.main').changes('main');
