69b6965469d46e2eca2e5a1c65591477e96bc60a
ampatspell <ampatspell@gmail.com>
2017-08-04 01:22:10 +0300

    remove fastboot check on changes listener start

--- app/instance-initializers/portfolio-changes.js
+++ app/instance-initializers/portfolio-changes.js
@@ -2,10 +2,6 @@ export default {
   name: 'portfolio:changes',
   after: 'portfolio:injections',
   initialize(app) {
-    let fastboot = app.lookup('service:fastboot');
-    if(fastboot.get('isFastBoot')) {
-      return;
-    }
     let changes = app.lookup('service:store').get('db.main').changes('main');
     changes.start();
   }
